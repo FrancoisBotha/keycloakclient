ae27d30b499c9a71cf8d71c2a63288921f4b60a4
FrancoisBotha <francois@ims.com.na>
2019-10-23 17:43:33 +1100

    Authenticate to Keycloak

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,24 +1,60 @@
 <template>
   <div class="hello">
-    <h1>{{ fruitsArray }}</h1>
+    <h1>OpenID Connect Client</h1>
+    <button v-on:click="authenticate">Authenticate</button>
+    <h3> Access Token: {{ AccessToken }}</h3>
+    <h4>{{ fruitsArray }}</h4>
   </div>  
 </template>
 
 <script>
 import axios from 'axios'
+import * as Keycloak from 'keycloak-js'
+
 export default {
   name: 'HelloWorld',
   data() {
     return {
-      fruitsArray: []
+      fruitsArray: [],
+      AccessToken: ""
     }
   },
   methods: {
+    authenticate: function() {
+
+      //keycloak init options
+      let kcOptions = {
+        url: 'http://localhost:8085/auth', 
+        realm: 'AuthSrvTest', 
+        clientId: 'vueclient'
+      }
+
+      let keycloak = Keycloak(kcOptions)
+
+      this.AccessToken = "Authenticating...."
+
+      keycloak.init({ onLoad: 'login-required' }).success((auth) =>{
+          
+          if(!auth) {
+            console.log("NOT Authenticated")
+            this.AccessToken = ""
+          } else {
+            console.log("Authenticated")
+            this.AccessToken = keycloak.token
+          }
+      
+          localStorage.setItem("vue-token", keycloak.token)
+          localStorage.setItem("vue-refresh-token", keycloak.refreshToken)
+
+      }).error(() =>{
+        console.log("Authenticated Failed")
+      });
+    },    
     getData: function() {
       var vm = this
       axios.get("http://localhost:3000/fruit", {
         headers: {
-          Authorization: 'Bearer SOME_TOKEN' 
+          Authorization: 'Bearer ' + localStorage.getItem('vue-token')
       }}).then(function(res) {
               vm.fruitsArray = res.data
             })
